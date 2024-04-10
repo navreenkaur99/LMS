3698050e7ef908142fcddbe4d0ab42c779d8ffdd
navreenkaur99 <160705709+navreenkaur99@users.noreply.github.com>
2024-04-10 17:20:08 +0530

    Add files via upload

--- Scond.jsx
+++ Scond.jsx
@@ -1,23 +1,46 @@
-
-
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Nav, NavLink, Navbar } from 'react-bootstrap';
 import QuizApp from './QuizApp';
 import Profile from './Profile';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Html from './Html'
-
+import Html from './Html';
+import Quiz from './Quiz';
+import Assignmentt from './Assignmentt';
 export default function Module() {
   const [expanded, setExpanded] = useState(false);
   const [activeModule, setActiveModule] = useState('chapter1');
   const [completedModules, setCompletedModules] = useState(0);
+  const [moduleCollapse, setModuleCollapse] = useState({
+    module1: false,
+    module2: false,
+    module3: false,
+    module4: false
+  });
+  
+    const [isRead, setIsRead] = useState(false);
+  
+    const handleMarkAsRead = () => {
+      setIsRead(true);
+      // Perform any additional logic, such as updating a database or state
+    };
+  
 
   const handleToggle = () => {
     setExpanded(!expanded);
   };
 
   const handleNavLinkClick = (module) => {
-    setActiveModule(module);
+     setActiveModule(module);
+  }
+  const handleNavLinkClick1 = (module) => {
+ 
+    // // Close other modules and toggle the clicked module
+    setModuleCollapse(prevState => ({
+      ...Object.keys(prevState).reduce((acc, key) => {
+        acc[key] = key === module ? !prevState[key] : false;
+           return acc;
+      }, {})
+    }));
   };
 
   useEffect(() => {
@@ -25,15 +48,12 @@ export default function Module() {
   }, []);
 
   useEffect(() => {
-    // Define an array of modules that are considered completion-worthy
-    const completionWorthyModules = ['tags', 'quiz', 'assignment']; // Add all modules here
-    // Count the number of completed modules
+    const completionWorthyModules = ['chapter1','tags', 'quiz', 'assignment','chapter2','Assignmentt'];
     const completedModulesCount = completionWorthyModules.includes(activeModule) ? completionWorthyModules.indexOf(activeModule) + 1 : 0;
     setCompletedModules(completedModulesCount);
   }, [activeModule]);
 
-  // Calculate completion percentage
-  const completionPercentage = ((completedModules / 3) * 100).toFixed(2); // Update the denominator based on the total number of completion-worthy modules
+  const completionPercentage = ((completedModules / 6) * 100).toFixed(2);
 
   return (
     <>
@@ -46,29 +66,32 @@ export default function Module() {
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="flex-column">
                   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }} className="sidebar-header">
-                    <svg style={{ marginLeft: "10px" }} xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+                    <svg style={{ marginLeft: "10px" }} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
                       <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
                     </svg>
-                    <h5 style={{ margin: '0', marginLeft: '20px' }}>Course content for you</h5>
+                    <h4 style={{ margin: '0', marginLeft: '65px' }}>   Course  content  for  you</h4>
                   </div>
+                   <hr></hr>
+                  <center>
                   <ul className="list-unstyled components">
                     <li>
-                      <NavLink href="#module1" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">
+                      <NavLink href="#module1" className="dropdown-toggle"   onClick={() => handleNavLinkClick1("module1")}>
                         Module1
                       </NavLink>
-                      <ul className="collapse list-unstyled" id="module1">
+                      <ul className={`list-unstyled ${moduleCollapse.module1 ? '' : 'collapse'}`} id="module1">
                         <li>
                           <Nav.Link href="#chapter1" onClick={() => handleNavLinkClick("chapter1")}>Html Introduction</Nav.Link>
                         </li>
                     </ul>
                     </li>
                   </ul>
+                  <hr></hr>
                   <ul className="list-unstyled components">
                     <li>
-                      <NavLink href="#module2" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">
+                      <NavLink href="#module2" className="dropdown-toggle" onClick={() => handleNavLinkClick1("module2")}>
                         Module2
                       </NavLink>
-                      <ul className="collapse list-unstyled" id="module2">
+                      <ul className={`list-unstyled ${moduleCollapse.module2 ? '' : 'collapse'}`} id="module2">
                         <li>
                           <Nav.Link onClick={() => handleNavLinkClick("tags")}>HTML tags</Nav.Link>
                         </li>
@@ -79,24 +102,39 @@ export default function Module() {
                       </ul>
                     </li>
                   </ul>
+                  <hr></hr>
                   <ul className="list-unstyled components">
                     <li>
-                      <NavLink href="#module3" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">
-                        Module
+                      <NavLink href="#module3" className="dropdown-toggle" onClick={() => handleNavLinkClick1("module3")}>
+                        Module3
                       </NavLink>
-                      <ul className="collapse list-unstyled" id="module3">
+                      <ul className={`list-unstyled ${moduleCollapse.module3 ? '' : 'collapse'}`} id="module3">
                         <li>
                           <Nav.Link  onClick={() => handleNavLinkClick("assignment")}>Assignment</Nav.Link>
                         </li>
                         <li>
-                          <Nav.Link href="#chapter2" onClick={() => handleNavLinkClick("chapter2")}>Chapter2</Nav.Link>
+                          <Nav.Link  onClick={() => handleNavLinkClick("chapter2")}>Quiz2</Nav.Link>
                         </li>
                       </ul>
                     </li>
                   </ul>
-                  <NavLink href="#module4" onClick={() => handleNavLinkClick("module4")}>
-                    Module4
-                  </NavLink>
+                  <hr></hr>
+                  <ul className="list-unstyled components">
+                    <li>
+                      <NavLink  className="dropdown-toggle" onClick={() => handleNavLinkClick1("module4")}>
+                        Module4
+                      </NavLink>
+                      <ul className={`list-unstyled ${moduleCollapse.module4 ? '' : 'collapse'}`} id="module4">
+                        <li>
+                          <Nav.Link  onClick={() => handleNavLinkClick("Assignmentt")}>Assignment</Nav.Link>
+                        </li>
+                        </ul>
+                    </li>
+                  </ul>
+                 
+                  
+                  </center>
+                 
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
@@ -117,20 +155,21 @@ export default function Module() {
                 </div>
               )}
             
-              {activeModule === "tags" && (<div style={{ width: '100%', height: '800px' }}>
+              {activeModule === "tags" && (<div style={{ width: '100%', height: '400px' }}>
                 <iframe
                   title="PDF Viewer"
                   src="https://web.stanford.edu/group/csp/cs21/htmlcheatsheet.pdf"
                   width="100%"
-                  height="50%"
+                  height="100%"
                 >
                   This browser does not support PDFs. Please download the PDF to view it.
                 </iframe>
               </div>)}
               {activeModule === "quiz" && <p><QuizApp /></p>}
               {activeModule === "assignment" && <p><Html/></p>}
+              {activeModule === "chapter2" && <p><Quiz/>  </p>}
 
-              {activeModule === "module4" && <p>Content for Module4</p>}
+              {activeModule === "Assignmentt" && <p><Assignmentt/></p>}
             </div>
             <hr />
             <p>Completion Percentage: {completionPercentage}%</p>
@@ -140,8 +179,6 @@ export default function Module() {
     </>
   );
 }
-
-
 
 
 
